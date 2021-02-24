d6c87a7ae96650b76d0cdbcaf03aa301078fd963
mathieugery <mathieu.gery@epitech.eu>
2021-02-25 00:17:15 +0100

    [+] Complete form modal

--- src/partials/addModal.js
+++ src/partials/addModal.js
@@ -14,21 +14,41 @@ function AddModal(props) {
                     className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
                     role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div>
-                        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                            <svg className="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M5 13l4 4L19 7"/>
-                            </svg>
-                        </div>
+
                         <div className="mt-3 text-center sm:mt-5">
                             <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
                                 Add Client
                             </h3>
-                            <div className="mt-2">
-                                <p className="text-sm text-gray-500">
-                                    Add your client here
-                                </p>
+                        </div>
+                        <div className="mt-10">
+                            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+                            <div className="mt-1">
+                                <input type="text" name="name" id="name"
+                                       className="h-10 p-2 shadow-sm ring-black focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-blue-600 rounded-md"
+                                       placeholder="Host1" aria-describedby="name-description"/>
+                            </div>
+                            <p className="mt-2 text-sm text-gray-500" id="name-description">Use friendly name for your host</p>
+                        </div>
+                        <div className="mt-10">
+                            <label for="host" class="block text-sm font-medium text-gray-700">Host adress</label>
+                            <div class="mt-1 p-2">
+                                <input type="text" name="host" id="host" class="h-10 p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="https://qbittorrent.yoursite.com or IP"/>
+                            </div>
+                        </div>
+                        <div className="mt-10">
+                            <label htmlFor="user" className="block text-sm font-medium text-gray-700">User</label>
+                            <div className="mt-1 p-2">
+                                <input type="text" name="user" id="user"
+                                       className="h-10 p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                       placeholder="admin"/>
+                            </div>
+                        </div>
+                        <div className="mt-10">
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                            <div className="mt-1 p-2">
+                                <input type="text" name="password" id="password"
+                                       className="h-10 p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                       placeholder="password"/>
                             </div>
                         </div>
                     </div>
