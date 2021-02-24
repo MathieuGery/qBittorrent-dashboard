02181761d373a442fa058c94c38d4293a3ad599d
mathieugery <mathieu.gery@epitech.eu>
2021-02-24 23:52:03 +0100

    [+] Show modal only when button pressed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 import React, { useState } from 'react';
 import Header from "./partials/header";
+import AddModal from "./partials/addModal";
 
 function App() {
 
+    const [showModal, setShowModal] = useState(false)
     const [statsList, setStatsList] = useState([]);
 
   return (
       <div className="flex flex-col min-h-screen overflow-hidden bg-gray-200">
-        <Header statsList={statsList} setStatsList={setStatsList} modal={setShowModal}/>
+        <Header statsList={statsList} setStatsList={setStatsList} setShowModal={setShowModal}/>
           <div>
               {statsList}
           </div>
-          <button type="button"
-                  onClick={(e) => {e.preventDefault();}}
-                  className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
-              Add
-          </button>
+          {showModal ? <AddModal setShowModal={setShowModal}/>:null}
     </div>
   );
 }
--- src/partials/header.js
+++ src/partials/header.js
@@ -15,8 +15,7 @@ function Header(props) {
     }, [top]);
 
     const addStatsComponent = event => {
-        props.setStatsList(props.statsList.concat(<Stats/>));
-        props.modal(true)
+        //props.setStatsList(props.statsList.concat(<Stats/>));
     };
 
     return (
@@ -35,7 +34,7 @@ function Header(props) {
                         Ygg website
                     </button>
                     <button type="button"
-                            onClick={(e) => {e.preventDefault(); addStatsComponent();}}
+                            onClick={(e) => {e.preventDefault(); addStatsComponent(); props.setShowModal(true)}}
                             className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
                         Add
                     </button>
